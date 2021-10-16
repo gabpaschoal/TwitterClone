70257c47e77bedd608a14d7c2271543e7c7d5e28
Guilherme Paschoal <gabpaschoal@outlook.com>
2021-10-16 17:35:12 -0300

    add base entity and audit

---
+++ Src/TwitterClone.Domain/Entities/Base/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿namespace TwitterClone.Domain.Entities.Base;
+
+public abstract class EntityBase : AuditBase
+{
+    public EntityBase()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; }
+}
