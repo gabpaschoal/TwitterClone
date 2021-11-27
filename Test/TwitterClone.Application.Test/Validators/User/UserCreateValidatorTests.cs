bc464d39c6bc89d5bcbe213e5ff025887f6486aa
Guilherme Paschoal <gabpaschoal@outlook.com>
2021-11-27 14:12:08 -0300

    test: Should add error when Name is null or empty

--- Test/TwitterClone.Application.Test/Validators/User/UserCreateValidatorTests.cs
+++ Test/TwitterClone.Application.Test/Validators/User/UserCreateValidatorTests.cs
@@ -1,6 +1,42 @@
-﻿namespace TwitterClone.Application.Test.Validators.User;
+﻿using FluentAssertions;
+using System.Linq;
+using TwitterClone.Application.Commands.User;
+using TwitterClone.Application.Validators.User;
+using Xunit;
+
+namespace TwitterClone.Application.Test.Validators.User;
 
 public class UserCreateValidatorTests
 {
+    private static UserCreateValidator MakeSut(
+        string name = "Gustav Jeremy",
+        string nickName = "gustav123",
+        string email = "gustav123@mail.com",
+        string password = "validPassword",
+        string passwordConfirmation = "validPassword")
+    {
 
+        UserCreateCommand user = new(name, nickName, email, password, passwordConfirmation);
+
+        UserCreateValidator validator = new();
+
+        validator.SetValue(user);
+        validator.Validate();
+
+        return validator;
+    }
+
+    [Fact(DisplayName = "Should add error when Name is null or empty")]
+    public void Should_add_error_when_Name_is_null_or_empty()
+    {
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+        var sutNull = MakeSut(name: null);
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+        sutNull.ResultData.IsValid.Should().BeFalse();
+        sutNull.ResultData.FieldErrors.Single().Key.Should().Be("Name");
+
+        var sutEmpty = MakeSut(name: "");
+        sutEmpty.ResultData.IsValid.Should().BeFalse();
+        sutEmpty.ResultData.FieldErrors.Single().Key.Should().Be("Name");
+    }
 }
