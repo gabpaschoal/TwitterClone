6f332ac12890fcdbf2d4a99aec683c44a511b6b2
Guilherme Paschoal <gabpaschoal@outlook.com>
2021-11-27 14:36:39 -0300

    test: Should add no error when Email is in the limit

--- Test/TwitterClone.Application.Test/Validators/User/UserCreateValidatorTests.cs
+++ Test/TwitterClone.Application.Test/Validators/User/UserCreateValidatorTests.cs
@@ -52,6 +52,18 @@ public class UserCreateValidatorTests
         sutMaxLenghtReq.ResultData.FieldErrors.Single().Key.Should().Be("Name");
     }
 
+    [Fact(DisplayName = "Should add no error when Name is in the limit")]
+    public void Should_add_no_error_when_Name_is_in_the_limit()
+    {
+        var sutMinLenghtReq = MakeSut(name: "".PadLeft(5, 'X'));
+        sutMinLenghtReq.ResultData.IsValid.Should().BeTrue();
+        sutMinLenghtReq.ResultData.FieldErrors.Should().BeEmpty();
+
+        var sutMaxLenghtReq = MakeSut(name: "".PadLeft(100, 'X'));
+        sutMaxLenghtReq.ResultData.IsValid.Should().BeTrue();
+        sutMaxLenghtReq.ResultData.FieldErrors.Should().BeEmpty();
+    }
+
     [Fact(DisplayName = "Should add error when NickName is null or empty")]
     public void Should_add_error_when_NickName_is_null_or_empty()
     {
@@ -78,6 +90,18 @@ public class UserCreateValidatorTests
         sutMaxLenghtReq.ResultData.FieldErrors.Single().Key.Should().Be("NickName");
     }
 
+    [Fact(DisplayName = "Should add no error when NickName is in the limit")]
+    public void Should_add_no_error_when_NickName_is_in_the_limit()
+    {
+        var sutMinLenghtReq = MakeSut(nickName: "".PadLeft(5, 'X'));
+        sutMinLenghtReq.ResultData.IsValid.Should().BeTrue();
+        sutMinLenghtReq.ResultData.FieldErrors.Should().BeEmpty();
+
+        var sutMaxLenghtReq = MakeSut(nickName: "".PadLeft(30, 'X'));
+        sutMaxLenghtReq.ResultData.IsValid.Should().BeTrue();
+        sutMaxLenghtReq.ResultData.FieldErrors.Should().BeEmpty();
+    }
+
     [Fact(DisplayName = "Should add error when Email is null or empty")]
     public void Should_add_error_when_Email_is_null_or_empty()
     {
@@ -90,5 +114,29 @@ public class UserCreateValidatorTests
         var sutEmpty = MakeSut(email: "");
         sutEmpty.ResultData.IsValid.Should().BeFalse();
         sutEmpty.ResultData.FieldErrors.Single().Key.Should().Be("Email");
+    }
+
+    [Fact(DisplayName = "Should add error when Email has less or more than the limit")]
+    public void Should_add_error_when_Email_has_less_or_more_than_the_limit()
+    {
+        var sutMinLenghtReq = MakeSut(email: "".PadLeft(4, 'X'));
+        sutMinLenghtReq.ResultData.IsValid.Should().BeFalse();
+        sutMinLenghtReq.ResultData.FieldErrors.Single().Key.Should().Be("Email");
+
+        var sutMaxLenghtReq = MakeSut(email: "".PadLeft(71, 'X'));
+        sutMaxLenghtReq.ResultData.IsValid.Should().BeFalse();
+        sutMaxLenghtReq.ResultData.FieldErrors.Single().Key.Should().Be("Email");
+    }
+
+    [Fact(DisplayName = "Should add no error when Email is in the limit")]
+    public void Should_add_no_error_when_Email_is_in_the_limit()
+    {
+        var sutMinLenghtReq = MakeSut(email: "".PadLeft(5, 'X'));
+        sutMinLenghtReq.ResultData.IsValid.Should().BeTrue();
+        sutMinLenghtReq.ResultData.FieldErrors.Should().BeEmpty();
+
+        var sutMaxLenghtReq = MakeSut(email: "".PadLeft(70, 'X'));
+        sutMaxLenghtReq.ResultData.IsValid.Should().BeTrue();
+        sutMaxLenghtReq.ResultData.FieldErrors.Should().BeEmpty();
     }
 }
