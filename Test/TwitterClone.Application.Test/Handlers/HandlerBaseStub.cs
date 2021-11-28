edee571eabae24375bef937a65427a6c97557e68
Guilherme Paschoal <gabpaschoal@outlook.com>
2021-11-28 10:35:16 -0300

    add method to check properties IsValid and IsInvalid

--- Test/TwitterClone.Application.Test/Handlers/HandlerBaseStub.cs
+++ Test/TwitterClone.Application.Test/Handlers/HandlerBaseStub.cs
@@ -11,11 +11,15 @@ public class StubCommand : IRequestExecution<Guid>
 
 public class HandlerBaseStub : HandlerBase<StubCommand, CustomResultData<Guid>>
 {
+    public int HandleExecutionCalls { get; private set; }
     public HandlerBaseStub(IHandlerBus handlerBus) : base(handlerBus)
-    { }
+    {
+        HandleExecutionCalls = 0;
+    }
 
     public override Task<CustomResultData<Guid>> HandleExecution(StubCommand request, CancellationToken cancellationToken)
     {
+        HandleExecutionCalls++;
         return Task.FromResult(new CustomResultData<Guid>());
     }
 }
---
+++ Test/TwitterClone.Application.Test/Handlers/HandlerBaseTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Moq;
+using TwitterClone.Application.Handlers;
+using Xunit;
+
+namespace TwitterClone.Application.Test.Handlers;
+
+public partial class HandlerBaseTests
+{
+    private static HandlerBaseStub MakeSut(IHandlerBus? handlerBus = null)
+    {
+        handlerBus ??= new Mock<IHandlerBus>().Object;
+
+        return new(handlerBus);
+    }
+
+    [Fact(DisplayName = "HandlerBase should be valid when is created")]
+    public void HandlerBase_should_be_valid_when_is_created_and_invalid_after_add_an_error()
+    {
+        var sut = MakeSut();
+
+        sut.IsValid.Should().BeTrue();
+        sut.ValidResponseAsync().IsValid.Should().BeTrue();
+        sut.InvalidResponseAsync().IsValid.Should().BeTrue();
+        sut.ValidResponse().Result.IsValid.Should().BeTrue();
+        sut.InvalidResponse().Result.IsValid.Should().BeTrue();
+        sut.IsInvalid.Should().BeFalse();
+
+        string key = "_key";
+        sut.AddError(key, "this key is invalid");
+
+        sut.IsValid.Should().BeFalse();
+        sut.ValidResponseAsync().IsValid.Should().BeTrue();
+        sut.IsInvalid.Should().BeTrue();
+        sut.InvalidResponseAsync().IsValid.Should().BeFalse();
+        sut.ValidResponse().Result.IsValid.Should().BeTrue();
+        sut.InvalidResponse().Result.IsValid.Should().BeFalse();
+        sut.HandleExecutionCalls.Should().Be(0);
+    }
+}
